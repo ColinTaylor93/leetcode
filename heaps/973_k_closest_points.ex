7e69006db2b5aaa069198205a480db08d90d5164
colintaylor93 <colintaylor15@gmail.com>
2025-12-23 21:33:35 -0500

    Changed min_heap to max_heap since that whats being used
    This is not present in my submitted solution, only here

--- heaps/973_k_closest_points.ex
+++ heaps/973_k_closest_points.ex
@@ -1,33 +1,33 @@
 defmodule Solution do
   @spec k_closest(points :: [[integer]], k :: integer) :: [[integer]]
   def k_closest(points, k) do
-    min_heap =
+    max_heap =
       points
       |> Enum.with_index()
-      |> Enum.reduce(:gb_trees.empty(), fn {[x, y], index}, min_heap_acc ->
+      |> Enum.reduce(:gb_trees.empty(), fn {[x, y], index}, max_heap_acc ->
         # Squared distance to origin (no sqrt needed)
         distance_squared = x * x + y * y
 
-        updated_min_heap =
+        updated_max_heap =
           :gb_trees.enter(
             {-distance_squared, index, x, y},
             true,
-            min_heap_acc
+            max_heap_acc
           )
 
-        # Keep min_heap size <= k
-        if :gb_trees.size(updated_min_heap) > k do
-          {_removed_key, _removed_val, trimmed_min_heap} =
-            :gb_trees.take_smallest(updated_min_heap)
+        # Keep max_heap size <= k
+        if :gb_trees.size(updated_max_heap) > k do
+          {_removed_key, _removed_val, trimmed_max_heap} =
+            :gb_trees.take_smallest(updated_max_heap)
 
-          trimmed_min_heap
+          trimmed_max_heap
         else
-          updated_min_heap
+          updated_max_heap
         end
       end)
 
     # Extract the k closest points
-    min_heap
+    max_heap
     |> :gb_trees.keys()
     |> Enum.map(fn {_neg_dist, _index, x, y} ->
       [x, y]
