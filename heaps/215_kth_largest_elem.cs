946e4b2fd4d527d52705418c746a7d1bcd0a2750
colintaylor93 <colintaylor15@gmail.com>
2026-01-10 23:35:55 -0500

    Added solution to 215

---
+++ heaps/215_kth_largest_elem.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        var heap = new PriorityQueue<int, int>();
+
+        foreach (int num in nums)
+        {
+            heap.Enqueue(num, num);
+
+            if (heap.Count > k)
+                heap.Dequeue();
+        }
+
+        return heap.Peek();
+    }
+}
